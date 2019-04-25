20b17ac5cf4b0e10409eda3aff2e6ce29470e379
Christian Dräger <christian-draeger1@gmail.com>
2019-04-25 22:55:48 +0200

    enable test

--- src/test/kotlin/it/skrape/ktor/KtorExtensionTest.kt
+++ src/test/kotlin/it/skrape/ktor/KtorExtensionTest.kt
@@ -15,7 +15,6 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
-@Disabled
 internal class KtorExtensionTest {
 
     private val aValidHtmlString = """
